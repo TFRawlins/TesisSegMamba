9495186e69eda27a1dde09f8c562935e1ad21a3d
TFRawlins <101351007+TFRawlins@users.noreply.github.com>
2025-07-01 19:31:08 -0400

    Update 3_train.py

--- 3_train.py
+++ 3_train.py
@@ -43,7 +43,7 @@ class LiverTrainer(Trainer):
         self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-4, weight_decay=1e-5)
         self.scheduler = LinearWarmupCosineAnnealingLR(self.optimizer, warmup_epochs=20, max_epochs=self.max_epochs)
         self.inferer = SlidingWindowInferer(
-            roi_size=[64, 64, 64],
+            roi_size=[96, 96, 96],
             sw_batch_size=1, 
         )
         self.scaler = torch.cuda.amp.GradScaler()
