f3a0624852db573e2a7aa3e550b2298feb6cd95e
TFRawlins <101351007+TFRawlins@users.noreply.github.com>
2025-07-01 18:47:45 -0400

    Update segmamba.py

--- model_segmamba/segmamba.py
+++ model_segmamba/segmamba.py
@@ -132,7 +132,7 @@ class GSC(nn.Module):
         return x + x_residual
 
 class MambaEncoder(nn.Module):
-    def __init__(self, in_chans=1, depths=[1, 1, 2, 2], dims=[16, 32, 64, 128],
+    def __init__(self, in_chans=1, depths=[2, 2, 2, 2], dims=[16, 32, 64, 128],
                  drop_path_rate=0., layer_scale_init_value=1e-6, out_indices=[0, 1, 2, 3]):
         super().__init__()
 
