fa2d95488780c4fac2f80c4d2329c35d4d0f8f7b
TFRawlins <101351007+TFRawlins@users.noreply.github.com>
2025-07-01 23:21:30 -0400

    Update dataset.py

--- light_training/dataloading/dataset.py
+++ light_training/dataloading/dataset.py
@@ -33,7 +33,7 @@ class MedicalDataset(Dataset):
         super().__init__()
         self.train_transforms = Compose([
             RandFlip(prob=0.5, spatial_axis=0),
-            RandAffined(prob=0.3, rotate_range=(0.1, 0.1, 0.1), scale_range=(0.1, 0.1, 0.1)),
+            RandAffined(keys=["image", "label"], prob=0.3, rotate_range=(0.1, 0.1, 0.1), scale_range=(0.1, 0.1, 0.1)),
             RandGaussianNoise(prob=0.2),
             RandScaleIntensity(factors=0.1, prob=0.3),
             NormalizeIntensity(nonzero=True, channel_wise=True),
