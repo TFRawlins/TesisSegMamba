0048373fb29b9864078fd4866530cf2c721a3d0c
TFRawlins <101351007+TFRawlins@users.noreply.github.com>
2025-08-29 13:24:22 -0400

    Update 3_train.py

--- 3_train.py
+++ 3_train.py
@@ -95,7 +95,7 @@ class LiverTrainer(Trainer):
         label = label.long()
         label = (label > 0).long()
         with torch.no_grad():
-            with torch.amp.autocast('cuda')           
+            with torch.amp.autocast('cuda'):         
                 logits = self.inferer(data, self.model)
             preds = torch.argmax(logits, dim=1)
             dice_value = dice(preds.cpu().numpy(), label.squeeze(1).cpu().numpy())
